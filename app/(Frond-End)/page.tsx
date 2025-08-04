957a884cf38de23db5748986a9b625c26aa5116b
utso <utsosarkar7870@gmail.com>
2025-08-04 15:10:35 +0600

    Interview section responsive and design done

--- app/(Frond-End)/page.tsx
+++ app/(Frond-End)/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/home/Hero";
+import InterviewSection from "@/components/home/InterviewSection";
 
 
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div className="">
    <Hero/>
+   <InterviewSection/>
     </div>
   );
 }
