623878b292a528d39f4845f1458c27c4a0d1fc1b
utso <utsosarkar7870@gmail.com>
2025-08-06 12:51:19 +0600

    Teastimonial section done

--- app/(Frond-End)/maids/page.tsx
+++ app/(Frond-End)/maids/page.tsx
@@ -1,9 +1,21 @@
+import Testimonials from "@/components/maids/Testimonials"
+import CommonHero from "@/components/reusable/CommonHero"
+import SectionHeader from "@/components/reusable/SectionHeader"
 
 function page() {
   return (
-    <div>
-      Maids
-    </div>
+<div>
+      <CommonHero pageName="Maids" />
+    <section className="my-14 lg:my-[100px]">
+      <div className="container">
+        <SectionHeader title="Success Stories from Other Maids" description="This section features real-life testimonials from foreign domestic workers (maids) who have successfully changed employers (transferred) with the help of a recruitment agency." />
+
+        <div>
+          <Testimonials/>
+        </div>
+      </div>
+      </section>
+</div>
   )
 }
 
