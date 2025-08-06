c2dba0c6ce290ccfb3125dafa2fb25043c07327b
utso <utsosarkar7870@gmail.com>
2025-08-06 11:03:06 +0600

    pricing contact done

--- app/(Frond-End)/pricing/page.tsx
+++ app/(Frond-End)/pricing/page.tsx
@@ -1,4 +1,5 @@
 import PaymentOption from '@/components/pricing/PaymentOption';
+import PriceDiscuse from '@/components/pricing/PriceDiscuse';
 import PricingPlan from '@/components/pricing/PricingPlan';
 import CommonHero from '@/components/reusable/CommonHero';
 import SectionHeader from '@/components/reusable/SectionHeader';
@@ -11,18 +12,22 @@ function page() {
         <div>
             <CommonHero pageName="Our Pricing" />
         </div>
-       <div className='py-10 md:py-14 lg:py-29'>
         <div className='container'>
+       <div className='py-10 md:py-14 lg:py-29'>
             <SectionHeader title='Payment Options' description='We offer flexible payment solutions to suit your needs' />
             <PaymentOption />
-        </div>
+       
        </div>
        <div className='pb-10 md:pb-14 lg:pb-29'>
-        <div className='container'>
+       
             <SectionHeader title='Flexible Pricing Options' description='Flexible payment options available. Contact us personally for a detailed discussion about payment plans that suit your needs.' />
             <PricingPlan />
-        </div>
+       
        </div>
+       <div className='pb-10 md:pb-14 lg:pb-29'>
+          <PriceDiscuse/>
+       </div>
+        </div>
     </div>
   )
 }
