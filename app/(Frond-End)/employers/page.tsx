405a5abdc593fda4e9049ae40e445933a7554ed7
utso <utsosarkar7870@gmail.com>
2025-08-07 12:42:16 +0600

    work section done

--- app/(Frond-End)/employers/page.tsx
+++ app/(Frond-End)/employers/page.tsx
@@ -1,6 +1,7 @@
 import CustomersSay from '@/components/employers/CustomersSay'
 
 import VideoCarousel from '@/components/employers/VideoCarousel'
+import Work from '@/components/employers/Work'
 import ComparisonTable from '@/components/maids/ComparisonTable'
 import CommonHero from '@/components/reusable/CommonHero'
 import SectionHeader from '@/components/reusable/SectionHeader'
@@ -14,6 +15,9 @@ function page() {
       <div className='py-14 lg:py-[120px] '>
         <SectionHeader title="Transfer Maid vs New Maid" description="To help you make an informed decision, here's a quick look at why hiring a transfer maid often makes more sense for Singaporean households:"/>
         <ComparisonTable/>
+      </div>
+      <div>
+        <Work/>
       </div>
       <div>
         <CustomersSay/>
