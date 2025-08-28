b331a6e8511f4d0d09c86d512e1b5d00558919e8
utso <utsosarkar7870@gmail.com>
2025-08-28 12:13:41 +0600

    price section done

--- app/(Frond-End)/pricing/page.tsx
+++ app/(Frond-End)/pricing/page.tsx
@@ -6,28 +6,27 @@ import SectionHeader from '@/components/reusable/SectionHeader';
 
 
 function page() {
-   
+
   return (
     <div>
-        <div>
-            <CommonHero pageName="Our Pricing" />
+      <div>
+        <CommonHero pageName="Our Pricing" />
+      </div>
+      <div className='container'>
+        <div className='py-10 md:py-14 lg:py-29'>
+          <SectionHeader title='For Employer Payment Options' description='Choose a payment plan that suits your budget — Installment options available for your convenience.' />
+          <PaymentOption />
         </div>
-        <div className='container'>
-       <div className='py-10 md:py-14 lg:py-29'>
-            <SectionHeader title='Payment Options' description='We offer flexible payment solutions to suit your needs' />
-            <PaymentOption />
-       
-       </div>
-       <div className='pb-10 md:pb-14 lg:pb-29'>
-       
-            <SectionHeader title='Flexible Pricing Options' description='Flexible payment options available. Contact us personally for a detailed discussion about payment plans that suit your needs.' />
-            <PricingPlan />
-       
-       </div>
-       <div className='pb-10 md:pb-14 lg:pb-29'>
-          <PriceDiscuse/>
-       </div>
+        <div className='pb-10 md:pb-14 lg:pb-29'>
+
+          <SectionHeader title='For Maids — No placement fee' description='We help you transfer to new employers with zero placement fees, making your transition smooth and affordable.' />
+          <PricingPlan />
+
         </div>
+        <div className='pb-10 md:pb-14 lg:pb-29'>
+          <PriceDiscuse />
+        </div>
+      </div>
     </div>
   )
 }
