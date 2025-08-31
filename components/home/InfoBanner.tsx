3338cf8c2ec988ede83bdc9f6e44c67c8d3e4e68
utso <utsosarkar7870@gmail.com>
2025-08-31 16:55:36 +0600

    update

--- components/home/InfoBanner.tsx
+++ components/home/InfoBanner.tsx
@@ -5,11 +5,26 @@ const InfoBanner = () => {
         "No Placement Fees for Maids! Register to transfer to new employer",
         "4 Months instalment payment option available for Employer",
     ];
+    const smItem = [
+        "Boarding House available, Reserve today",
+        "No Placement Fees for Maids!",
+        "4 Months instalment payment ",
+    ];
 
     return (
         <div className="w-full py-3 md:py-5 overflow-x-auto" style={{ backgroundImage: "url('/home/topbg.png')", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
-            <div className="flex gap-4 justify-center px-4 min-w-max mx-auto">
+            <div className="xl:flex gap-4 justify-center hidden  px-4 min-w-max mx-auto">
                 {items.map((text, idx) => (
+                    <div
+                        key={idx}
+                        className="bg-secondaryColor px-5 py-3 rounded-xl shadow-sm text-sm md:text-base font-medium text-headerColor whitespace-nowrap"
+                    >
+                        {text}
+                    </div>
+                ))}
+            </div>
+            <div className="flex xl:hidden gap-4 justify-center px-4 min-w-max mx-auto">
+                {smItem.map((text, idx) => (
                     <div
                         key={idx}
                         className="bg-secondaryColor px-5 py-3 rounded-xl shadow-sm text-sm md:text-base font-medium text-headerColor whitespace-nowrap"
