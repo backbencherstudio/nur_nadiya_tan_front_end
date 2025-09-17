5caa523149ebc2935fb03605ab96ded84f15650f
utso <utsosarkar7870@gmail.com>
2025-09-17 14:37:49 +0600

    update

--- app/(Frond-End)/page.tsx
+++ app/(Frond-End)/page.tsx
@@ -3,6 +3,7 @@ import InfoBanner from "@/components/home/InfoBanner";
 import InterviewSection from "@/components/home/InterviewSection";
 import JobList from "@/components/home/JobList";
 import MaidsHero from "@/components/home/MaidsHero";
+import PriceDiscuse from "@/components/pricing/PriceDiscuse";
 
 
 
@@ -14,6 +15,11 @@ export default function Home() {
       <InterviewSection />
       <MaidsHero />
       <JobList />
+      <div className="container">
+        <div className='pb-10 md:pb-14 lg:pb-29   '>
+          <PriceDiscuse />
+        </div>
+      </div>
     </div>
   );
 }
